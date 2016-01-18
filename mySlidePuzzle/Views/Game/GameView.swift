3eda988b7dae9368caa70c97cf7d52c4613f6a8e
hiroshings <hiroshings.job1987@gmail.com>
2016-01-18 13:29:10 +0900

    .xibに切り替える途中まで

---
+++ mySlidePuzzle/Views/Game/GameView.swift
@@ -0,0 +1,21 @@
+//
+//  GameView.swift
+//  mySlidePuzzle
+//
+//  Created by 坂本 浩 on 2016/01/18.
+//  Copyright © 2016年 hiroshings. All rights reserved.
+//
+
+import UIKit
+
+class GameView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
